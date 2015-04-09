7b485812bcfc00295f7a7a2de4262826f78d94dd
adayan <adayan2009@sina.com>
2015-04-09 22:15:17 +0800

    fix bug

--- js/main.js
+++ js/main.js
@@ -1,26 +1,69 @@
 var MAIN = (function($) {
   'use strict';
 
-  var $view, el, aboutSetting;
+  var $view, el, aboutSetting,
+        anchors = ['about', 'skill', 'page1'];
 
 
   function cacheElements() {
     el = {
-      $aboutNum: $view.find('.js-num')
+      $sections: $view.find('.section'),
+      $aboutNum: $view.find('.js-num'),
+      $canvas: document.getElementById('canvas')
     };
   }
 
+  function hasAnimated(index) {
+    return el.$sections.eq(index).hasClass('js-animated');
+  }
 
+  function isFirstSection() {
+    return location.hash.substr(1) === anchors[0];
+  }
 
   function initFullpage() {
     $view.fullpage({
       sectionsColor: ['#7BAABE', '#F8823C', '#003366', '#f90'],
-      anchors: ['about', 'skill', 'page1'],
-      menu: '#menu'
+      anchors: anchors,
+      menu: '#menu',
+      paddingTop: 50,
+      afterLoad: function(anchorLink , index) {
+        if (!hasAnimated(index - 1)) {
+         doAnimation(index);
+        }
+      }
     });
   }
 
-  function initFirstSection() {
+  function doAnimation(index) {
+    switch(index) {
+      case 1:
+        initAbout();
+        break;
+
+      case 2:
+        initSkill();
+        break;
+
+      case 3:
+        break;
+
+      default:
+        break;
+    }
+
+    el.$sections.eq(index - 1) .addClass('js-animated')
+                        .find('.animated').removeClass('hidden');
+  }
+
+  /** About Section **/
+  function initAbout() {
+    setTimeout(function() {
+        initAboutAnimation();  // init first section
+      }, 2000);
+  }
+
+  function initAboutAnimation() {
     var speed = 500,
         count = el.$aboutNum.length,
         numbers = [2, 9, 100];
@@ -42,16 +85,54 @@ var MAIN = (function($) {
     }
   }
 
+  /** Skill Section **/
+  function initSkill() {
+    var skills = {
+
+    };
+    drawProcessCirle();
+  }
+
+  function drawProcessCirle() {
+    var canvas = el.$canvas;
+    var ctx =  canvas.getContext('2d');
+
+    // outter cirle
+    ctx.beginPath();
+    ctx.arc(150, 150, 100, 0, Math.PI*2, true);
+    ctx.lineWidth=25;
+    ctx.strokeStyle="lightblue";
+    ctx.stroke();
+    ctx.closePath();
+
+    // angle
+    var count = 0;
+    var startAngle = 3*Math.PI/2;
+    var handle = setInterval(function() {
+        if (count == 10) {
+            clearInterval(handle);
+        }
+
+        // angle
+        ctx.beginPath();
+        ctx.arc(150, 150, 100, startAngle, startAngle + (2*Math.PI*count/(2*10)), false);
+        ctx.strokeStyle="darkblue";
+        ctx.stroke();
+        //ctx.closePath();
+
+        count++;
+    }, 60);
+
+  }
 
   return {
     init: function() {
       $view = $('.section-wrap');
       cacheElements();
-
       initFullpage();
-      setTimeout(function() {
-        initFirstSection();  // init first section
-      }, 2000);
+      if (isFirstSection()) {
+        doAnimation(1);
+      }
     }
   };
 }(jQuery));
